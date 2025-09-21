00b2941bd74a4784d959bdddbbd8ccf402a729ad
aren125 <ren24007@byui.edu>
2025-09-21 00:00:22 -0400

    prep 3 finsihed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int numberOfGuesses = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            numberOfGuesses = numberOfGuesses + 1;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations! The Magic Number was {magicNumber}. You guessed it in {numberOfGuesses} tries.");
+            }
+
+        }
+
+        Console.WriteLine("Thanks for playing!");
+
     }
 }
