9630b949c272f9146ba3fcec6ad70f9bfd388f07
aren125 <ren24007@byui.edu>
2025-09-30 20:13:41 -0400

    Step 5: Creating Getters and Setters and testing

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -8,5 +8,14 @@ class Program
         Fraction f2 = new Fraction(6);
         Fraction f3 = new Fraction(6, 7);
 
+        Console.WriteLine($"{f1.GetTop()}/{f1.GetBottom()}");
+        Console.WriteLine($"{f2.GetTop()}/{f2.GetBottom()}");
+        Console.WriteLine($"{f3.GetTop()}/{f3.GetBottom()}");
+
+        f1.SetTop(3);
+        Console.WriteLine($"{f1.GetTop()}/{f1.GetBottom()}");
+
+        f1.SetBottom(4);
+        Console.WriteLine($"{f1.GetTop()}/{f1.GetBottom()}");
     }
 }
