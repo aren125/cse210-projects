dd88987ec473c0b49275d32edb27e6d9e2c39a32
aren125 <ren24007@byui.edu>
2025-10-02 22:13:59 -0400

    Realizing the console.writeline was unneccessary in the Program file. Also created the Scripture and Word files

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,7 +7,7 @@ class Program
         Reference r1 = new Reference("John", 3, 16);
         Reference r2 = new Reference("Proverbs", 3, 5, 6);
 
-        Console.WriteLine(r1.DisplayReference()); //Test for reference
-        Console.WriteLine(r2.DisplayReference()); // Test for reference
+        r1.DisplayReference(); //Test for reference
+        r2.DisplayReference(); // Test for reference
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    Reference _reference;
+    public List<Word> _words = new List<Word>();
+    
+}
