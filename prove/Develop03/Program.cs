11bc796752a2330c1fdb3a021290ef781811527e
aren125 <ren24007@byui.edu>
2025-10-02 21:53:49 -0400

    Reference class created and tested in program class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference r1 = new Reference("John", 3, 16);
+        Reference r2 = new Reference("Proverbs", 3, 5, 6);
+
+        Console.WriteLine(r1.DisplayReference()); //Test for reference
+        Console.WriteLine(r2.DisplayReference()); // Test for reference
     }
 }
