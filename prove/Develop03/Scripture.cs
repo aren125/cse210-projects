983abde7342f1c32f5d9962c12bff6309508ccad
aren125 <ren24007@byui.edu>
2025-10-03 22:53:24 -0400

    Included visible words list so the random hidden words do not repeat. Finished program

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -6,7 +6,6 @@ public class Scripture
 {
     private Reference _reference;
     private List<Word> _words = new List<Word>(); //each word will be an object
-
     public Scripture(Reference reference, string text) // Constructor for Scripture
     {
         _reference = reference;
@@ -33,10 +32,26 @@ public class Scripture
     public void HideRandomWords(int count) //randomly hide words
     {
         Random random = new Random();
-        for (int i = 0; i < count; i++)
+        List<Word> visibleWords = new List<Word>(); //visible words-not hidden
+
+        foreach (Word word in _words)
         {
-            int index = random.Next(_words.Count);
-            _words[index].HideWord(); //HideWord function will be called from the word class
+            if (word.IsHidden() == false)
+            {
+                visibleWords.Add(word);
+            }
+        }
+
+        if (count > visibleWords.Count)
+        {
+            count = visibleWords.Count; //Change count to be the same number as the left over words
+        }
+
+        for (int i = 0; i < count; i++) //count?
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].HideWord(); //HideWord function will be called from the word class
+            visibleWords.RemoveAt(index);
         }
     }
     public bool AllWordsHidden() //boolean to quit program if true
