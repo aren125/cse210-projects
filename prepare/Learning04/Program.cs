3ec0059719f7e01f587536e971032aa45260158c
aren125 <ren24007@byui.edu>
2025-10-07 20:13:34 -0400

    mathassignment class and test

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -7,5 +7,10 @@ class Program
         Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
         Console.WriteLine();
         Console.WriteLine(a1.GetSummary());
+
+        MathAssignment m1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine();
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
     }
 }
