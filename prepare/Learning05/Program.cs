63efbb44c3cc54c0845b8e20fa02a8dd7a4aa6de
aren125 <ren24007@byui.edu>
2025-10-15 17:36:48 -0400

    circle and rectangle classes and test

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,8 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Square s1 = new Square("Blue", 5);
-
+        Square s1 = new Square("blue", 5);
         Console.WriteLine($"{s1.GetColor()}, {s1.GetArea()}");
+
+        Rectangle r1 = new Rectangle("red", 4, 5);
+        Console.WriteLine($"\n{r1.GetColor()}, {r1.GetArea()}");
+
+        Circle c1 = new Circle("yellow", 6);
+        Console.WriteLine($"\n{c1.GetColor()}, {c1.GetArea().ToString("F2")}");
     }
 }
