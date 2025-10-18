7133fb9add53ab6b618ff31245a36a84e2634191
aren125 <ren24007@byui.edu>
2025-10-18 00:49:42 -0400

    Goal Manager- Load Goals, save goals, and convert to goal from file string

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.Marshalling;
 
 public class GoalManager
 {
@@ -29,15 +30,74 @@ public class GoalManager
 
     public void SaveGoals(string fileName)
     {
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (Goal goal in _goals)
+            {
+                outputFile.WriteLine(goal.GetStringRepresentation());
+            }
+            outputFile.WriteLine($"Score:{_score}"); //save the score
+        }
+        Console.WriteLine("Your goals have been saved.");
 
     }
 
     public void LoadGoals(string fileName)
     {
+        _goals.Clear();
+        string[] lines = File.ReadAllLines(fileName);
+
+        foreach (string line in lines)
+        {
+            if (line.StartsWith("Score:"))
+            {
+                _score = int.Parse(line.Split(':')[1]);
+            }
+            else
+            {
+                Goal g = CreateGoalFromString(line);
+                _goals.Add(g);
+            }
+            Console.WriteLine("Your goals have been loaded.");
+        }
 
     }
     public void DisplayScore()
     {
         Console.WriteLine($"You have {_score} points.");
+    }
+
+    public static Goal CreateGoalFromString(string line)
+    {
+        string[] parts = line.Split(':');
+        string type = parts[0];
+        string[] details = parts[1].Split(',');
+        if (type == "SimpleGoal")
+        {
+            SimpleGoal sg = new SimpleGoal(details[0], details[1], int.Parse(details[2]));
+            if (bool.Parse(details[3]))
+            {
+                sg.RecordEvent(); // if the bool is true- if the goal has been completed, check the box
+            }
+            return sg;
+        }
+        else if (type == "EternalGoal")
+        {
+            return new EternalGoal(details[0], details[1], int.Parse(details[2]));
+        }
+        else if (type == "ChecklistGoal")
+        {
+            ChecklistGoal cg = new ChecklistGoal(details[0], details[1], int.Parse(details[2]), int.Parse(details[4]), int.Parse(details[5]));
+            for (int i = 0; i < int.Parse(details[3]); i++)
+            {
+                cg.RecordEvent();
+            }
+            return cg;
+        }
+        else
+        {
+            throw new Exception("Unknown goal type");
+        }
+        
     }
 }
