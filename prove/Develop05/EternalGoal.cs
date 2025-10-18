264ec721001c05bb7eb9ecc3a68c7fd7e07ac656
aren125 <ren24007@byui.edu>
2025-10-17 23:47:56 -0400

    EternalGoal class

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class EternalGoal : Goal
+{
+    public EternalGoal(string name, string description, int points) : base(name, description, points)
+    { }
+
+    public override void RecordEvent()
+    {
+        Console.WriteLine($"Congratulations, you have earned {GetPoints()} points!"); //math/score done in goal manager
+    }
+
+    public override string GetStatus()
+    {
+        return "[ ]";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"EternalGoal:{GetName()},{GetDescription()}, {GetPoints()}";
+    }
+}
