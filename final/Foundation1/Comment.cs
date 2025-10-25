7ba039e3a2d889a3ad84a03e1b22740096b4c025
aren125 <ren24007@byui.edu>
2025-10-24 21:20:53 -0400

    comment class

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+public class Comment
+{
+    private string _commenterName;
+    private string _comment;
+
+    public Comment(string commenterName, string comment)
+    {
+        _commenterName = commenterName;
+        _comment = comment;
+    }
+
+    public void DisplayComment()
+    {
+        Console.WriteLine($"{_commenterName}: {_comment}");
+    }
+}
